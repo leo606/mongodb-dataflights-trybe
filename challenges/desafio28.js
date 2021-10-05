1fabbda2f5b8f86a5017abbaab5a9a267d3478c7
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-05 10:30:55 -0300

    desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,22 @@
+const EMPRESA_NAME = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne({
+  empresa: EMPRESA_NAME,
+  totalVoosDomesticos: db.voos.find(
+  {
+    "empresa.nome": EMPRESA_NAME,
+    natureza: "Doméstica",
+  },
+).count(),
+});
+
+db.resumoVoos.findOne(
+  {
+    empresa: EMPRESA_NAME,
+  },
+  {
+    _id: 0,
+    empresa: 1,
+    totalVoosDomesticos: 1,
+  },
+);
